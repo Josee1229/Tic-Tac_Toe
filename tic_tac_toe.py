71b2c7d74833ffbe4f2c5c99c0058b0ce7f7c03e
Josee1229 <jsanyamgupta@gmail.com>
2026-04-02 18:20:19 +0530

    Added X and O printing capabilities to the buttons.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -14,11 +14,17 @@ titleLabel.pack()
 frame2 = tk.Frame(root)
 frame2.pack()
 
+turn = 'x'
+
 def play(event):
+    global turn
     button = event.widget
-
-    button["text"] = 'JS'
-
+    if turn == 'x':
+        button["text"] = 'x'
+        turn = 'o'
+    else:
+        button["text"] = 'o'
+        turn = 'x'
 
 
 # Tic-tac-toe Board Creation
@@ -30,29 +36,37 @@ button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 0, column = 1)
+button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 0, column = 2)
+button1.bind("<Button-1>", play)
 
 #2nd row
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 1, column = 0)
+button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 1, column = 1)
+button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 1, column = 2)
+button1.bind("<Button-1>", play)
 
 #3rd row
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 3, column = 0)
+button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 3, column = 1)
+button1.bind("<Button-1>", play)
 
 button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
 button1.grid(row = 3, column = 2)
+button1.bind("<Button-1>", play)
 
 
 
