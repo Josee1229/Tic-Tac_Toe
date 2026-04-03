4cc187f3345aea1fde6bcb422aefc93d2efb6c57
Josee1229 <jsanyamgupta@gmail.com>
2026-04-03 06:19:08 +0530

    Added winning and draw logic.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,26 +5,84 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("500x500")
 root.title("Tic Tac Toe")
+root.configure(bg='pink')
 
 frame1 = tk.Frame(root)
 frame1.pack()
 titleLabel = tk.Label(frame1, text = "Tic Tac Toe", font=("Montserrat", 25), bg="pink", fg="green")
-titleLabel.pack()
+titleLabel.grid(row=0, column=0)
 
 frame2 = tk.Frame(root)
 frame2.pack()
 
+board = {1:" ", 2:" ", 3:" ",
+        4:" ", 5:" ", 6:" ",
+        7:" ", 8:" ", 9:" "
+         }
+
 turn = 'x'
+
+def checkForWin(player):
+    if (board[1] == board[2] and board[2] == board[3] and board[3] == player):
+        return True
+    elif (board[4] == board[5] and board[5] == board[6] and board[6] == player):
+        return True
+    elif (board[7] == board[8] and board[8] == board[9] and board[9] == player):
+        return True
+    elif (board[1] == board[4] and board[4] == board[7] and board[7] == player):
+        return True
+    elif (board[2] == board[5] and board[5] == board[8] and board[8] == player):
+        return True
+    elif (board[3] == board[6] and board[6] == board[9] and board[9] == player):
+        return True
+    elif (board[1] == board[5] and board[5] == board[9] and board[9] == player):
+        return True
+    elif (board[3] == board[5] and board[5] == board[7] and board[7] == player):
+        return True
+    else:
+        return False;
+
+
+def checkForDraw():
+    for i in board.keys():
+        if board[i] == ' ':
+            return False
+    return True
+
 
 def play(event):
     global turn
     button = event.widget
-    if turn == 'x':
-        button["text"] = 'x'
-        turn = 'o'
+
+    buttonTxt = str(button)
+    buttonIdentifier = buttonTxt[-1]
+    if buttonIdentifier == 'n':
+        clicked = 1
     else:
-        button["text"] = 'o'
-        turn = 'x'
+        clicked = int(buttonIdentifier)
+
+
+
+    if button["text"] == " ":
+        if turn == 'x':
+            button["text"] = 'x'
+            board[clicked] = turn
+            if checkForWin(turn):
+                winningLabel = tk.Label(frame1, text=f"{turn} wins the game!",font=('Montserrat',25),bg='pink',fg='green')
+                winningLabel.grid(row=0,column=0,columnspan=3)
+            turn = 'o'
+        else:
+            button["text"] = 'o'
+            board[clicked] = turn
+            if checkForWin(turn):
+                winningLabel = tk.Label(frame1, text=f"{turn} wins the game!",font=('Montserrat',25),bg='pink',fg='green')
+                winningLabel.grid(row=0, column=0, columnspan=3)
+            turn = 'x'
+
+        if checkForDraw():
+            drawLabel = tk.Label(frame1, text="Game Draw!", font=('Montserrat', 25), bg='pink',fg='green')
+            drawLabel.grid(row=0, column=0, columnspan=3)
+
 
 
 # Tic-tac-toe Board Creation
@@ -34,39 +92,42 @@ button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserra
 button1.grid(row = 0, column = 0)
 button1.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 0, column = 1)
-button1.bind("<Button-1>", play)
+button2 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button2.grid(row = 0, column = 1)
+button2.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 0, column = 2)
-button1.bind("<Button-1>", play)
+button3 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button3.grid(row = 0, column = 2)
+button3.bind("<Button-1>", play)
 
 #2nd row
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 1, column = 0)
-button1.bind("<Button-1>", play)
+button4 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button4.grid(row = 1, column = 0)
+button4.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 1, column = 1)
-button1.bind("<Button-1>", play)
+button5 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button5.grid(row = 1, column = 1)
+button5.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 1, column = 2)
-button1.bind("<Button-1>", play)
+button6 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button6.grid(row = 1, column = 2)
+button6.bind("<Button-1>", play)
 
 #3rd row
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 3, column = 0)
-button1.bind("<Button-1>", play)
+button7 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button7.grid(row = 3, column = 0)
+button7.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 3, column = 1)
-button1.bind("<Button-1>", play)
+button8 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button8.grid(row = 3, column = 1)
+button8.bind("<Button-1>", play)
 
-button1 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
-button1.grid(row = 3, column = 2)
-button1.bind("<Button-1>", play)
+button9 = tk.Button(frame2, text = " ", width = 4, height = 2,font = ("Montserrat",30),bg="green", relief=tk.RAISED, borderwidth=5)
+button9.grid(row = 3, column = 2)
+button9.bind("<Button-1>", play)
+
+restartButton = tk.Button(frame2, text="Restart Game", width=12, height=1, font=("Montserrat",20),bg="green", relief=tk.RAISED, borderwidth=5)
+restartButton.grid(row = 4, column = 0,columnspan = 3)
 
 
 
